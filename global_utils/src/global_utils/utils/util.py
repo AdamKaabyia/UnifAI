8e9743860239673e4c6617b688875b01f94e3896
labuyous <labuyous@redhat.com>
2026-04-29 11:40:51 -0400

    remove pss

--- global_utils/src/global_utils/utils/util.py
+++ global_utils/src/global_utils/utils/util.py
@@ -38,10 +38,9 @@ def get_temporal_url():
 def get_redis_url():
     ip = config.redis_ip
     port = config.redis_port
-    password = "Mc10vin!!"
     if not ip:
         return ""
-    return f"redis://:{password}@{ip}:{port}"
+    return f"redis://{ip}:{port}"
 
 
 def get_rabbitmq_url(user=None, password=None):
