56a181ee789d05b54795308127ff1b378c6a7d5f
sfiresht <saar.firshtein.ext@nokia.com>
2026-05-05 12:20:17 +0300

    initial testing for vault plugin

---
+++ ci/test-vault.groovy
@@ -0,0 +1,35 @@
+properties([
+    parameters([
+        // 🌐 Global Parameters
+        string(name: 'VAULT_SECRET_PATH', defaultValue: 'automation-and-tools/unifai/redis', description: 'Vault secret path')
+        string(name: 'VAULT_SECRET_KEY', defaultValue: 'redis_port', description: 'Key within the secret')
+    ])
+])
+
+pipeline {
+    agent any
+
+    stages {
+        stage('Read Secret from Vault') {
+            steps {
+                withVault(
+                    configuration: [
+                        vaultUrl: '',           // leave empty to use global config
+                        vaultCredentialId: ''   // leave empty to use global config
+                    ],
+                    vaultSecrets: [
+                        [
+                            path: "${params.VAULT_SECRET_PATH}",
+                            secretValues: [
+                                [envVar: 'MY_SECRET', vaultKey: "${params.VAULT_SECRET_KEY}"]
+                            ]
+                        ]
+                    ]
+                ) {
+                    sh 'echo "Secret retrieved (masked): $MY_SECRET"'
+                    // Use MY_SECRET env var in your steps here
+                }
+            }
+        }
+    }
+}
