4adb097b53695fa8ce595744cdbcc756cdd76b3f
sfiresht <saar.firshtein.ext@nokia.com>
2026-05-05 18:37:28 +0300

    another try

--- ci/test-vault.groovy
+++ ci/test-vault.groovy
@@ -35,22 +35,41 @@ pipeline {
                 }
             }
         }
-        stage('Read Secret key from Vault using vault cli') {
+        stage('Read Secret key from Vault using approle') {
             steps {
-              withCredentials([string(credentialsId: 'vault_creds', variable: 'VAULT_TOKEN')]) {
+            withCredentials([
+                usernamePassword(
+                    credentialsId: 'creds for rh corporate vault service',
+                    usernameVariable: 'ROLE_ID',
+                    passwordVariable: 'SECRET_ID'
+                )
+            ]) {
                 script {
+                    def token = sh(
+                        script: '''
+                            export VAULT_ADDR='https://vault.corp.redhat.com:8200/'
+                            export VAULT_SKIP_VERIFY=true
+                            vault write -field=token auth/approle/login \
+                                role_id=$ROLE_ID \
+                                secret_id=$SECRET_ID
+                        ''',
+                        returnStdout: true
+                    ).trim()
                     def json = sh(
-                    script: "vault kv get -format=json ${params.VAULT_SECRET_PATH}",
-                    returnStdout: true
+                        script: """
+                            export VAULT_ADDR='https://vault.corp.redhat.com:8200/'
+                            export VAULT_SKIP_VERIFY=true
+                            export VAULT_TOKEN=${token}
+                            vault kv get -format=json ${params.VAULT_SECRET_PATH}
+                        """,
+                        returnStdout: true
                     ).trim()
                     def secrets = readJSON text: json
-                    def data = secrets.data.data  // KV v2; use secrets.data for KV v1
-                    data.each { key, value -> echo "Key: ${key}"}
+                    def data = secrets.data.data
+                    data.each { key, value -> echo "Key: ${key}" }
                 }
-
-              }
             }
         }
-        
+        }
     }
 }
