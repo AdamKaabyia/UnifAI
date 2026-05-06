a9f63ae9bf6f7af66cc618b54de7b037c9c0dfe5
sfiresht <saar.firshtein.ext@nokia.com>
2026-05-06 09:56:44 +0300

    commented out simple step

--- ci/test-vault.groovy
+++ ci/test-vault.groovy
@@ -5,7 +5,7 @@ properties([
         string(name: "BRANCH", defaultValue: "GENIE-948_hashicorp_integration", description: "Git branch to build images from."),
         
         // 🔒 Vault Parameters
-        string(name: 'VAULT_SECRET_PATH', defaultValue: 'apps/automation-and-tools/unifai/redis', description: 'Vault secret path'),
+        string(name: 'VAULT_SECRET_PATH', defaultValue: 'apps/automation-and-tools/unifai', description: 'Vault secret path'),
         string(name: 'VAULT_SECRET_KEY', defaultValue: 'redis_port', description: 'Key within the secret')
     ])
 ])
@@ -26,28 +26,28 @@ def secret_lists = [
 pipeline {
     agent any
 
-    stages {
-        stage('Read Secret key from Vault natively') {
-            steps {
-                withVault(
-                    configuration: [
-                        vaultUrl: '',           // leave empty to use global config
-                        vaultCredentialId: ''   // leave empty to use global config
-                    ],
-                    vaultSecrets: [
-                        [
-                            path: "${params.VAULT_SECRET_PATH}",
-                            secretValues: [
-                                [envVar: 'MY_SECRET', vaultKey: "${params.VAULT_SECRET_KEY}"]
-                            ]
-                        ]
-                    ]
-                ) {
-                    sh 'echo "Secret retrieved (masked): $MY_SECRET"'
-                    // Use MY_SECRET env var in your steps here
-                }
-            }
-        }
+    // stages {
+    //     stage('Read Secret key from Vault natively') {
+    //         steps {
+    //             withVault(
+    //                 configuration: [
+    //                     vaultUrl: '',           // leave empty to use global config
+    //                     vaultCredentialId: ''   // leave empty to use global config
+    //                 ],
+    //                 vaultSecrets: [
+    //                     [
+    //                         path: "${params.VAULT_SECRET_PATH}",
+    //                         secretValues: [
+    //                             [envVar: 'MY_SECRET', vaultKey: "${params.VAULT_SECRET_KEY}"]
+    //                         ]
+    //                     ]
+    //                 ]
+    //             ) {
+    //                 sh 'echo "Secret retrieved (masked): $MY_SECRET"'
+    //                 // Use MY_SECRET env var in your steps here
+    //             }
+    //         }
+    //     }
         stage('Read Secret key permodule') {
             steps {
                 secret_lists.each { module, secrets ->
