781b1b47daa9ebb683c8fac0de005c80e2da36d8
sfiresht <saar.firshtein.ext@nokia.com>
2026-05-07 12:48:37 +0300

    missing secret

--- ci/pipeline-deploy-vault.groovy
+++ ci/pipeline-deploy-vault.groovy
@@ -43,7 +43,7 @@ def secret_lists = [
     rabbitmq: ['rmq_username', 'rmq_password'],
     umami: ['umami_username', 'umami_password'],
     // keycloak: ['keycloak_base_url', 'client_id', 'client_secret', 'keycloak_realm'],
-    global_config: [ 'secret_key'],
+    global_config: ['secret_key'],
     multiagent: ['CREDENTIAL_ENCRYPTION_KEY', 'MCP_AUTH_STATE_SECRET'],
     rag: ['default_slack_bot_token', 'default_slack_user_token'],
     ]
