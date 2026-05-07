87fe63bf76fc1328c8477dc81e2afce51fb778ee
MayaCrmi <your_email@example.com>
2026-05-07 04:33:48 -0400

    back

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -38,14 +38,14 @@ export default defineConfig({
       },
       // Proxy for api2 (assuming this is still local or another service)
       '/api2': {
-        target: 'http://10.46.254.113:8002', // Your second backend
+        target: process.env.MULTIAGENT_HOST,//'http://127.0.0.1:13457', // Your second backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api2/, '/api'), // This rewrites /api2 to nothing
         // secure: false, // Only needed if this target is HTTPS and you have SSL issues
       },
       // Proxy for api2 (assuming this is still local or another service)
       '/api3': {
-        target: process.env.MULTIAGENT_HOST,//'http://127.0.0.1:13457', // Your second backend
+        target: process.env.IDENTITY_HOST,//'http://127.0.0.1:13457', // Your second backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api3/, '/api'), // This rewrites /api2 to nothing
         // secure: false, // Only needed if this target is HTTPS and you have SSL issues
