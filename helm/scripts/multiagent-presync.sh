05df9294fd652848d89a40d407ca16a1d467cebd
labuyous <labuyous@redhat.com>
2026-06-01 04:01:19 -0400

    naming

--- helm/scripts/multiagent-presync.sh
+++ helm/scripts/multiagent-presync.sh
@@ -8,4 +8,5 @@ source "$(dirname "$0")/postsync-lib.sh"
 #create secret
 create_or_update_resource "secret generic" multiagent-be-secret \
   --from-literal=CREDENTIAL_ENCRYPTION_KEY="$CREDENTIAL_ENCRYPTION_KEY" \
-  --from-literal=MCP_AUTH_STATE_SECRET="$MCP_AUTH_STATE_SECRET"
+  --from-literal=OAUTH_STATE_SECRET="$OAUTH_STATE_SECRET"
+
