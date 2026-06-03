96b14d53b17d547e47a68dfe009d8fee2e8ea113
sfiresht <saar.firshtein.ext@nokia.com>
2026-06-03 17:28:44 +0300

    updates for deployment

--- helm/scripts/identity-postsync.sh
+++ helm/scripts/identity-postsync.sh
@@ -9,6 +9,6 @@ IDENTITY_PORT=$(wait_for_port identity) || exit 1
 IDENTITY_IP=$(wait_for_service_name identity) || exit 1
 
 create_or_update_configmap identity-svc-config \
-  --from-literal=IDENTITY_ADDR="$IDENTITY_ADDR" \
-  --from-literal=IDENTITY_PORT="$IDENTITY_PORT" \
-  --from-literal=IDENTITY_IP="$IDENTITY_IP"
+  --from-literal=IDENTITY_SVC_ADDR="$IDENTITY_ADDR" \
+  --from-literal=IDENTITY_SVC_PORT="$IDENTITY_PORT" \
+  --from-literal=IDENTITY_SVC_IP="$IDENTITY_IP"
